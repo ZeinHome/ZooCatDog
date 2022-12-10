f061dafcfc8f4e345b8ff083472582f7148d859a
Ilya Pyatnichuk <zein3288@gmail.com>
2022-12-10 23:56:20 +0200

    all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
+// import { useRef } from 'react';
+// import { FaArrowUp } from 'react-icons/fa';
+// import { scrollToTop } from '../js/scrollToTop';
 import { Routes, Route } from 'react-router-dom';
 import { useMedia } from 'react-use';
-import { useRef } from 'react';
-import { FaArrowUp } from 'react-icons/fa';
-import { scrollToTop } from '../js/scrollToTop';
 import { Container } from './Container/Container.styled';
 
 import Home from '../pages/Home';
@@ -18,35 +18,35 @@ import ListProduct from './ListProduct';
 
 import { listProduct } from '../js/FakeAPI/listProduct'; //mono delait
 
-export const GoTop = styled.button`
-  position: fixed;
-  right: 30px;
-  bottom: 30px;
-  display: inline-block;
-  width: 64px;
-  height: 64px;
-  margin: 0;
-  padding: 5px;
-  border: none;
-  opacity: 0;
-  background: linear-gradient(
-    90deg,
-    rgba(249, 231, 254, 1) 35%,
-    rgba(218, 252, 252, 1) 100%
-  );
-  transition: background-position 1s ease-out;
-  border-radius: 50%;
-  z-index: 10;
-  cursor: pointer;
-  :hover {
-    background-position: 100px;
-  }
-  svg {
-    :hover {
-      fill: red;
-    }
-  }
-`;
+// export const GoTop = styled.button`
+//   position: fixed;
+//   right: 30px;
+//   bottom: 30px;
+//   display: inline-block;
+//   width: 64px;
+//   height: 64px;
+//   margin: 0;
+//   padding: 5px;
+//   border: none;
+//   opacity: 0;
+//   background: linear-gradient(
+//     90deg,
+//     rgba(249, 231, 254, 1) 35%,
+//     rgba(218, 252, 252, 1) 100%
+//   );
+//   transition: background-position 1s ease-out;
+//   border-radius: 50%;
+//   z-index: 10;
+//   cursor: pointer;
+//   :hover {
+//     background-position: 100px;
+//   }
+//   svg {
+//     :hover {
+//       fill: red;
+//     }
+//   }
+// `;
 
 export const App = () => {
   const isWide = useMedia('(min-width: 1200px)');
